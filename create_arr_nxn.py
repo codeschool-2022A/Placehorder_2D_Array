76fbfae340ff891c05c211b95df628e7ba28e31f
Jalilov <javohirjalilov777@gmail.com>
2022-03-22 19:36:27 +0500

    added tasks

---
+++ create_arr_nxn.py
@@ -0,0 +1,9 @@
+def create_arr_nxn(n,k):
+  """
+  Create 2D array NxN of k
+  Args:
+    int: n
+    int: k
+  Returns:
+    list: 2D list
+  """
---
+++ create_arr_ones.py
@@ -0,0 +1,9 @@
+def create_arr_ones(n,m):
+  """
+  Create 2D array NxM of ones
+  Args:
+    int: n
+    int: m
+  Returns:
+    list: 2D list
+  """
